346abf3027201909c2989e3d61a9d8143472500a
Bao Bong Bong <inam.bao.ttb@gmail.com>
2024-10-09 14:15:52 +0700

    DOM event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,20 @@ class MyComponent extends React.Component {
         age: '26'
     }
 
+    handleClick(event) {
+        // console.log(">>Click me my button")
+        console.log(">>My name is ", this.state.name)
+    }
+    handleMouseOver(event) {
+        console.log(event.pageX)
+    }
     //JSX
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                <button onMouseOver={this.handleMouseOver}>Hover Me</button>
+                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
