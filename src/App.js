d1fe5d33b7fcf3a7a87519b30a0b6bc25b1aab51
Bao Bong Bong <inam.bao.ttb@gmail.com>
2024-10-30 15:17:25 +0700

    Routes/ Route, Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test hello world
+        <div>
+          <button>
+            <Link to="/users">go to user test</Link>
+          </button>
+          <button>
+            <Link to="/admins">go to admin test</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
