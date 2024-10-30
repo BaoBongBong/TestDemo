83e1b93708c856d9fabe4e9816c5e6d55738310d
Bao Bong Bong <inam.bao.ttb@gmail.com>
2024-10-30 23:26:44 +0700

    design video homepage

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -1,7 +1,14 @@
+import videoHomePage1 from '../../assets/video-homepage1.mp4'
+import videoHomePage2 from '../../assets/video-homepage2.mp4'
+
 const HomePage = (props) => {
     return (
         <div className="hoempage-container">
-            home page container
+            <video autoPlay muted loop>
+                <source src={videoHomePage2}
+                    type='video/mp4'
+                />
+            </video>
         </div>
     )
 }
